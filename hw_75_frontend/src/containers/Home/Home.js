aac5f180841e8e40caf48d7e2e9571c561b5ec13
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-14 14:46:29 +0600

    add Home page

---
+++ hw_75_frontend/src/containers/Home/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import CodeForm from "../../components/CodeForm/CodeForm";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <CodeForm/>
+            </div>
+        );
+    }
+}
+
+export default Home;
